26696431dba3fca09aecc1c486159edf3bbbe43e
hgs-study <hgstudy_@naver.com>
2021-01-05 00:12:33 +0900

    [SpringBoot] 그레이들 새로고침

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -42,3 +42,6 @@ nohup java -jar \
 #  -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
 #  -Dspring.profiles.active=real \
 #  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
+
+# JAR 파일 여러개 두면 구동 안됨
