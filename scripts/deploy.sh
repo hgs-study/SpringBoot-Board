b0cb8335051b7dd15702ebae56156ed0a39b78df
hgs-study <hgstudy_@naver.com>
2021-01-04 21:05:42 +0900

    [SpringBoot] Travis CI + S3 + codeDeploy -> EC2 설정 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step3
-PROJECT_NAME=SpringBoot-Board
+PROJECT_NAME=board_test
+#PROJECT_NAME=SpringBoot-Board
 
 echo "> Build 파일 복사"
 
@@ -9,16 +10,16 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl SpringBoot-Board | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl board_test | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
 fi
 
 echo "> 새 애플리케이션 배포"
